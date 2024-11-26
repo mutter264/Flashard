1d3c94925ad3e38c03b4edf3fef3f90a26b05f05
mutter264 <gabymutter10@gmail.com>
2024-11-25 23:22:15 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,57 @@
+criaCartao(
+    'Física',
+    'Qual é a velocidade da luz?',
+    '299.792.458 m/s'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a rio mais longo do mundo?',
+    'Rio Amazonas'
+)
+
+criaCartao(
+    'Ciências',
+    'Quantos ossos temos no corpo?',
+    '206'
+)
+criaCartao(
+    'Matemática',
+    'Qual é a teorama de Pitágoras?',
+    'a²+b²=c²'
+) 
+criaCartao(
+    'Ciência',
+    'Qual é olimite da capacidade do cérebro humano?',
+    'Estimado 1 exabyte (1 bilhão de gigabytes)'
+) 
+criaCartao(
+    'Historia',
+    'Quem foi o lider da revolução Russa em 1917?',
+    'Vladimir Lenin'
+) 
+criaCartao(
+    'artes',
+    'Qual é o estilo artístico caracterizado por formas geometricas?',
+    'culbismo'
+) 
+criaCartao(
+    'literatura',
+    'Quais são as caracteristicas do romantismo?',
+    'Ênfase na emoção, imaginação e individualismo'
+) 
+criaCartao(
+    'cultura nerd',
+    'Quantos anéis foram entregues para cada classe em "senhos dos anéis de J.R.R. Tolkien?',
+    'Três Anéis para os Reis-Elfos sob este céu, Sete para os Senhores-Anões em seus rochosos corredores, Nove para os Homens Mortais fadados ao eterno sono, Um para o Senhor do Escuro em seu escuro trono. Portanto, 20 anéis.'
+) 
+criaCartao(
+    'cultura nerd',
+    'quantos pokemons tem na pokedex?',
+    'Tem 1015 pokemons'
+) 
+criaCartao(
+    'Tecnologia',
+    'O que é Deep Learning?',
+    'O deep learning ou aprendizagem profunda baseia-se no machine learning para, a partir de uma grande quantidade de dados e após inúmeras camadas de processamento com algoritmos, conseguir que um computador aprenda por si mesmo e execute tarefas semelhantes às dos seres humanos, tais como a identificação de imagens, o reconhecimento de voz ou a realização de predições, de forma progressiva.'
+) 
